36edaacb9146212e09a9d68e2989ae09103b8a84
Noel Welsh <noelwelsh@gmail.com>
2010-12-16 10:51:27 +0000

    Remove duplicates from data. Extract preprocessing to its own function

--- data.r
+++ data.r
@@ -11,4 +11,15 @@ rawData = read.delim('ltpp-data.txt', header=TRUE, sep='\t')
 # Select the first set of variables we're interested in, scale it, and drop NAs
 #
 # Dependent variables is column 63 in raw data, column 1 in this data
-trafficData = as.data.frame(na.exclude(normalise(rawData[,c(63,10,11,12)], 1)))
+trafficData = preprocess(rawData[,c(63,10,11,12)], 1)
+
+
+# preprocess(Data.Frame) -> Data.Frame
+#
+# Perform standard preprocessing on data:
+# - normalise
+# - exclude NaNs
+# - exclude duplicates
+preprocess = function(data) {
+  as.data.frame(unique(na.exclude(normalise(data))))
+}
